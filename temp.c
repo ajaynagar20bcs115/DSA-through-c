1b65800eb0902f97d0770e1b9b6c647b11b1dd44
ajay nagar123 <81499008+ajaynagar20bcs115@users.noreply.github.com>
2021-06-12 07:18:13 +0530

    Add files via upload

---
+++ temp.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<math.h>
+#define sci(n) scanf("%d",&n)
+#define true printf("True")
+#define jumpline printf("\n")
+#define pri(n) printf("%d",n)
+#define prs(s) printf("%s",s)
+#define spc printf(" ")
+#define false printf("False")
+#define fo(i,n) for(int i=0;i<n;i++)
+#define start int main(){
+#define end }
+typedef struct node{
+int data;
+struct node* next;}node;
+start
+int n;sci(n);pri(n);
+end
+
+
+
+
